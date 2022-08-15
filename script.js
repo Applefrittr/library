1e09f333d4b1500f311295a9dbddc16f6750efa5
applefrittr <jacob.lee.jordan@gmail.com>
2022-08-15 18:49:19 -0400

    initial modal implementation

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const addBtn = document.querySelector('.add-book')
 const shelves = document.querySelector('.books')
 const clearBtn = document.querySelector('.clear')
+const modal = document.querySelector('.modal')
+const modalAdd = document.querySelector('.modal-add')
+const bookForm = document.querySelector('.book-form')
 
 addBtn.addEventListener('click', () => {
-    const book = document.createElement('div')
-    book.classList.add('book-card')
-
-    shelves.appendChild(book)
+    modal.style.display = 'block'
 })
 
 clearBtn.addEventListener('click', () => {
@@ -14,3 +14,17 @@ clearBtn.addEventListener('click', () => {
         shelves.removeChild(shelves.firstChild)
     }          
 })
+
+modalAdd.addEventListener('click', () => {
+    let elements = bookForm.entries()
+    
+    console.log(elements)
+    alert(elements[0].value)
+})
+
+// addBtn.addEventListener('click', () => {
+//     const book = document.createElement('div')
+//     book.classList.add('book-card')
+
+//     shelves.appendChild(book)
+// })
